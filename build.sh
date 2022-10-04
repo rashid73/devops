3aa441607ff4c53104bbd30a9bbdb354981da277
rashid73 <31983224+rashid73@users.noreply.github.com>
2022-10-04 17:49:12 +0500

    Update build.sh
    add a new comment

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ tag=$(date +%Y-%m-%d-%H-%M)
 echo $tag
 docker build -t raxhid/admin:$tag .
 #docker login -u raxhid -p tfc@30mcl
+#test login with stdin
 docker login --username raxhid --password-stdin
 docker push raxhid/admin:$tag
 echo "All OK"
