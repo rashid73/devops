e0dc6f49fb45f320d3e4e651c9974d251307f2c4
rashid73 <31983224+rashid73@users.noreply.github.com>
2022-10-04 17:44:53 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 tag=$(date +%Y-%m-%d-%H-%M)
 echo $tag
 docker build -t raxhid/admin:$tag .
-docker login -u raxhid -p tfc@30mcl
+#docker login -u raxhid -p tfc@30mcl
+docker login --username raxhid --password-stdin
 docker push raxhid/admin:$tag
 echo "All OK"
 #echo "done with build and pushed images to the repository"
