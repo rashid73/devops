6ef4fe9a3a6e6d5c7a1f2a5676f350434632ddf3
rashid73 <31983224+rashid73@users.noreply.github.com>
2022-10-05 14:59:31 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ echo $tag
 docker build -t raxhid/admin:$tag .
 #docker login -u raxhid -p tfc@30mcl
 #teting secrete file and login
-docker login --username raxhid --password-stdin --no-include-email
+docker login --username raxhid --password-stdin
 docker push raxhid/admin:$tag
 echo "All OK"
 #echo "done with build and pushed images to the repository"
