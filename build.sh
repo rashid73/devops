d215665e154cf224c4a79527a3fcedff51d39da5
rashid73 <31983224+rashid73@users.noreply.github.com>
2022-10-14 10:46:29 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #tag=$(date +%Y-%m-%d-%H-%M)
 tag=$(echo $GIT_BRANCH   | sed -e "s|origin/||g")-${BUILD_NUMBER}
 echo $tag
-docker build -t raxhid/admin:$tag .
+docker build -t raxhid/server:$tag .
 #docker login -u raxhid -p tfc@30mcl
 #teting secrete file and login
 cat password.txt | docker login -u raxhid --password-stdin
-docker push raxhid/admin:$tag
+docker push raxhid/server:$tag
 echo "All OK"
 #echo "done with build and pushed images to the repository"
 echo "Message changed"
